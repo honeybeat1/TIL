a8c203e3bcc61082262bdd8abb440a36e2a407d5
Lana Chung <lana@Lanaui-MacBookPro.local>
2020-07-23 23:38:26 +0900

    add header

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dachung <dachung@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/23 23:29:07 by dachung           #+#    #+#             */
+/*   Updated: 2020/07/23 23:37:50 by dachung          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+
+#endif
