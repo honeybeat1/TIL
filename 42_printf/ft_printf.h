31c6da5f2a91036f64d4ee69210b014378ecb3d2
Lana Chung <lana@Lanaui-MacBookPro.local>
2020-08-16 14:43:12 +0900

    add initial files

---
+++ 42_printf/ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dachung <dachung@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/07 19:48:54 by dachung           #+#    #+#             */
+/*   Updated: 2020/08/07 20:04:33 by dachung          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <stdarg.h>
+# include <unistd.h>
+
+typedef struct s_info
+{
+	int			flag;
+
+};
